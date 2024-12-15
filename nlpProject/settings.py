62a268270eaf0bbda40b451c4a20a8396b9474ff
Isha Ghaisas <ighaisas@iu.edu>
2024-12-15 14:08:44 -0500

    deployment changes

--- nlpProject/settings.py
+++ nlpProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-qg*yf7z6+fc2gy^p6##xk2ktk4r)$30t=)eqqz@3+(zvhr9(d$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
